79e119cb4f52ac2b7a2f9bd2f334d2b76491d19b
44mira <legolaslada@gmail.com>
2024-12-01 15:48:46 +0800

    feat: c answer

--- day1/day1.c
+++ day1/day1.c
@@ -1,5 +1,28 @@
+#include "vector.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int part1(Vector *left, Vector *right) {
+  Vector *sortedLeft = left->sort(left, lessThan);
+  Vector *sortedRight = right->sort(right, lessThan);
+
+  int diff = 0, sum = 0;
+  for (int i = 0; i < sortedLeft->size; i++) {
+    sum += abs(sortedLeft->array[i] - sortedRight->array[i]);
+  }
+
+  return sum;
+}
+
+int part2(Vector *left, Vector *right) {
+  int item, sum = 0;
+  for (int i = 0; i < left->size; i++) {
+    item = left->array[i];
+    sum += right->count(right, item) * item;
+  }
+
+  return sum;
+}
 
 int main(int argc, char **argv) {
   // use cmd arg argument
@@ -9,7 +32,25 @@ int main(int argc, char **argv) {
   }
 
   FILE *f = fopen(filename, "r");
+  if (f == NULL) {
+    printf("Error reading %s.\n", filename);
+    return 1;
+  }
 
-  fclose(f);
+  Vector *left = new_vec();
+  Vector *right = new_vec();
+
+  int n_left, n_right;
+  char buffer[BUFFER];
+  while (fgets(buffer, BUFFER, f)) {
+    sscanf(buffer, "%d   %d", &n_left, &n_right);
+
+    left->append(left, n_left);
+    right->append(right, n_right);
+  }
+
+  printf("%d\n", part1(left, right));
+  printf("%d\n", part2(left, right));
+
   return 0;
 }
---
+++ day1/vector.h
@@ -0,0 +1,25 @@
+#ifndef VECTOR
+#define VECTOR
+
+#define SCALING_FACTOR 2
+#define BUFFER 500
+
+typedef unsigned int uint;
+typedef struct Vector Vector;
+typedef struct Vector {
+  int *array;
+  uint size;
+  uint maxSize;
+  void (*append)(Vector *, int);
+  Vector *(*sort)(Vector *v, int (*sortfunc)(int, int));
+  uint (*count)(Vector *, int);
+} Vector;
+
+Vector *new_vec();
+void append(Vector *v, int elem);
+uint count(Vector *right, int elem);
+
+int lessThan(int a, int b);
+int greaterThan(int a, int b);
+
+#endif
