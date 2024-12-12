f765da0a80ff38c4e1dcb41f596acdf998c67c7a
44mira <legolaslada@gmail.com>
2024-12-13 04:35:10 +0800

    feat: day 12 part 2 attempt 1

--- 2024/day12/py/day12.py
+++ 2024/day12/py/day12.py
@@ -21,10 +21,31 @@ def out_of_bound(grid: npt.NDArray, pt: point):
     return x < 0 or x >= x_bound or y < 0 or y >= y_bound
 
 
+def get_perimeter(pt, ydr, xdr, bulk_x, bulk_y) -> int:
+    if bulk_x is None or bulk_y is None:
+        return 1
+
+    y, x = pt
+
+    # vertical side
+    if ydr != 0:
+        if y + ydr in bulk_y:
+            return 0
+        bulk_y.add(ydr + y)
+        return 1
+    # horizontal side
+    if x + xdr in bulk_x:
+        return 0
+    bulk_x.add(x + xdr)
+    return 1
+
+
 def find_dimension(
     grid: npt.NDArray,
     pt: point | Any,
     covered_points: set[point],
+    bulk_x=None,
+    bulk_y=None,
 ):
     if pt in covered_points:
         return 0, 0
@@ -36,12 +57,10 @@ def find_dimension(
 
     for ydr, xdr in adjacent_squares():
         adj_pt = (y + ydr, x + xdr)
-        if out_of_bound(grid, adj_pt):
-            perimeter += 1
-        elif grid[adj_pt] != grid[pt]:
-            perimeter += 1
+        if out_of_bound(grid, adj_pt) or grid[adj_pt] != grid[pt]:
+            perimeter += get_perimeter(pt, ydr, xdr, bulk_x, bulk_y)
         elif adj_pt not in covered_points:
-            a, p = find_dimension(grid, adj_pt, covered_points)
+            a, p = find_dimension(grid, adj_pt, covered_points, bulk_x, bulk_y)
             area += a
             perimeter += p
 
@@ -56,7 +75,23 @@ def part1(grid: npt.NDArray):
         if pt in covered_points:
             continue
         a, p = find_dimension(grid, pt, covered_points)
-        print(grid[pt], a * p)
+        result += a * p
+
+    return result
+
+
+def part2(grid: npt.NDArray):
+    covered_points: set[point] = set()
+
+    result = 0
+    for pt in np.ndindex(np.shape(grid)):
+        if pt in covered_points:
+            continue
+        bulk_x, bulk_y = set(), set()
+        a, p = find_dimension(grid, pt, covered_points, bulk_x, bulk_y)
+
+        print(bulk_x, bulk_y)
+        print(grid[pt], p, a * p)
         result += a * p
 
     return result
@@ -78,6 +113,7 @@ def main():
 
     print(grid)
     print(part1(grid))
+    print(part2(grid))
 
 
 if __name__ == "__main__":
